775163e7182d00aa0542d33d5bb2e996290881ed
Blake Fletcher <blkfltchr@gmail.com>
2018-12-06 11:35:47 -0800

    Fixed project card sizing

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,14 +12,14 @@ const Projects = () => {
     return (
         <div>
             <Header />
-            <div style={{margin: "5rem auto 0 auto", height: "100%", textAlign: "center"}}>
+            <div style={{margin: "4.5rem auto 0 auto", height: "100%", textAlign: "center"}}>
                 <h2 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "2rem"}}>Projects.</h2>
                 <div style={{display: "flex", justifyContent: "space-evenly"}}>
-                    <div style={{width: "500px", margin: "0.5rem", padding: "0.5rem 0", border: "solid 1px lightgrey", borderRadius: "1.5rem"}}>
-                        <img src={howtolearnreact} alt="howtolearnreact logo"/>
+                    <div style={{width: "370px", margin: "0.5rem", padding: "0.5rem 0", border: "solid 1px lightgrey", borderRadius: "1rem"}}>
+                        <img style={{width: "370px"}} src={howtolearnreact} alt="howtolearnreact logo"/>
                         <div style={{textAlign: "center"}}>
-                            <h4 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "1.5rem"}}>How To Learn React</h4>
-                            <p style={{fontFamily: "Merriweather,Georgia,Times New Roman,serif", fontSize: "1.2rem", textAlign: "center"}}>A project intended to share the things I learn while working with React. As I build things, complete tutorials, read articles, and watch videos, I add to the blog.</p>
+                            <h4 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "1.4rem"}}>How To Learn React</h4>
+                            <p style={{fontFamily: "Merriweather,Georgia,Times New Roman,serif", fontSize: "1.1rem", textAlign: "center"}}>A project intended to share the things I learn while working with React. As I build things, complete tutorials, read articles, and watch videos, I add to the blog.</p>
                             <div>
                                 <img style={{height: "25px", margin: "0.5rem"}} src={gatsby} alt="gatsby logo"/>
                                 <img style={{height: "25px", margin: "0.5rem"}} src={graphql} alt="graphql logo"/>
