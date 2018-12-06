b3344ad14dc94f0584950167d887f4c5cccd22d0
Blake Fletcher <blkfltchr@gmail.com>
2018-12-05 22:36:27 -0800

    Added first project card

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import howtolearnreact from '../images/howtolearnreact-logo-rectangle.png'
+import gatsby from '../images/gatsby-logo-rectangle.png'
+import graphql from '../images/graphql-logo-rectangle.png'
+import contentful from '../images/contentful-logo-rectangle.png'
+import styled from '../images/styled-logo-square.png'
+import algolia from '../images/algolia-logo-rectangle.png'
 
 const Projects = () => {
     return (
         <div>
             <Header />
-            <div style={{margin: "0 auto", width: "75vw", height: "100%", display: "flex", flexDirection: "column", textAlign: "center"}}>
+            <div style={{margin: "0 auto", height: "100%", textAlign: "center"}}>
                 <h2 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "2rem"}}>Projects.</h2>
+                <div style={{display: "flex", justifyContent: "space-evenly"}}>
+                    <div style={{width: "500px", margin: "0.5rem", padding: "0.5rem 0", border: "solid 1px lightgrey", borderRadius: "1.5rem"}}>
+                        <img src={howtolearnreact} alt="howtolearnreact logo"/>
+                        <div style={{textAlign: "center"}}>
+                            <h4 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "1.5rem"}}>How To Learn React</h4>
+                            <p style={{fontFamily: "Merriweather,Georgia,Times New Roman,serif", fontSize: "1.2rem", textAlign: "center"}}>A project intended to share the things I learn while working with React. As I build things, complete tutorials, read articles, and watch videos, I add to the blog.</p>
+                            <div>
+                                <img style={{height: "25px", margin: "0.5rem"}} src={gatsby} alt="gatsby logo"/>
+                                <img style={{height: "25px", margin: "0.5rem"}} src={graphql} alt="graphql logo"/>
+                                <img style={{height: "25px", margin: "0.5rem"}} src={contentful} alt="contentful logo"/>
+                                <img style={{height: "25px", margin: "0.5rem"}} src={styled} alt="styled logo"/>
+                                <img style={{height: "25px", margin: "0.5rem"}} src={algolia} alt="algolia logo"/>
+                            </div>
+                            <div style={{fontFamily: "Merriweather,Georgia,Times New Roman,serif", fontSize: "1.2rem"}}>
+                                <a style={{margin: "0.5rem"}} href="https://github.com/blkfltchr/howtolearnreact" target="_blank" rel="noopener noreferrer">Code</a>
+                                <a style={{margin: "0.5rem"}} href="https://www.howtolearnreact.com/" target="_blank" rel="noopener noreferrer">Website</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
