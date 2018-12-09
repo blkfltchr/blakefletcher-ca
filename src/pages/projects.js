5c566fb11caf4ff0a7cb0830a77fd1b88f0e057b
Blake Fletcher <blkfltchr@gmail.com>
2018-12-09 15:30:45 -0500

    Added buttons to project card on hover

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import '../css/page-styles.css'
 import howtolearnreact from '../images/howtolearnreact-logo-rectangle.png'
 import gatsby from '../images/gatsby-logo-rectangle.png'
 import graphql from '../images/graphql-logo-rectangle.png'
@@ -16,7 +17,27 @@ const Projects = () => {
                 <h2 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "2rem"}}>Projects.</h2>
                 <div style={{display: "flex", justifyContent: "space-evenly"}}>
                     <div style={{width: "370px", margin: "0.5rem", padding: "0.5rem 0", border: "solid 1px lightgrey", borderRadius: "1rem"}}>
-                        <img style={{width: "370px"}} src={howtolearnreact} alt="howtolearnreact logo"/>
+                        <div className="project-card">
+                            <img style={{width: "370px"}} src={howtolearnreact} alt="howtolearnreact logo"/>
+                            <div className="project-buttons">
+                                <a 
+                                    className="project-button"
+                                    style={{margin: "0.5rem"}} 
+                                    href="https://github.com/blkfltchr/howtolearnreact" 
+                                    target="_blank" 
+                                    rel="noopener noreferrer">
+                                    <button>Code</button>
+                                </a>
+                                <a 
+                                    className="project-button"
+                                    style={{margin: "0.5rem"}} 
+                                    href="https://www.howtolearnreact.com/" 
+                                    target="_blank" 
+                                    rel="noopener noreferrer">
+                                    <button>Website</button>
+                                </a>
+                            </div>
+                        </div>
                         <div style={{textAlign: "center"}}>
                             <h4 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "1.4rem"}}>How To Learn React</h4>
                             <p style={{fontFamily: "Merriweather,Georgia,Times New Roman,serif", fontSize: "1.1rem", textAlign: "center"}}>A project intended to share the things I learn while working with React. As I build things, complete tutorials, read articles, and watch videos, I add to the blog.</p>
@@ -27,11 +48,25 @@ const Projects = () => {
                                 <img style={{height: "25px", margin: "0.5rem"}} src={styled} title="Styled Components" alt="styled logo"/>
                                 <img style={{height: "25px", margin: "0.5rem"}} src={algolia} alt="algolia logo"/>
                             </div>
-                            <div style={{fontFamily: "Merriweather,Georgia,Times New Roman,serif", fontSize: "1.2rem"}}>
-                                <a style={{margin: "0.5rem"}} href="https://github.com/blkfltchr/howtolearnreact" target="_blank" rel="noopener noreferrer">Code</a>
-                                <a style={{margin: "0.5rem"}} href="https://www.howtolearnreact.com/" target="_blank" rel="noopener noreferrer">Website</a>
-                            </div>
                         </div>
+                        <div>
+                                <a 
+                                    className="project-button"
+                                    style={{margin: "0.5rem"}} 
+                                    href="https://github.com/blkfltchr/howtolearnreact" 
+                                    target="_blank" 
+                                    rel="noopener noreferrer">
+                                    <button>Code</button>
+                                </a>
+                                <a 
+                                    className="project-button"
+                                    style={{margin: "0.5rem"}} 
+                                    href="https://www.howtolearnreact.com/" 
+                                    target="_blank" 
+                                    rel="noopener noreferrer">
+                                    <button>Website</button>
+                                </a>
+                            </div>
                     </div>
                 </div>
             </div>
