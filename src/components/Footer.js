f07b1be43548e1d8cb8009c7db15cb53703b2253
Blake Fletcher <blkfltchr@gmail.com>
2018-12-09 14:04:17 -0500

    Added linkedin to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import goodreads from '../images/goodreads-logo.png'
 import medium from '../images/medium-logo.png'
 import twitter from '../images/twitter-logo.png'
 import messenger from '../images/messenger-logo.png'
+import linkedin from '../images/linkedin-logo.png'
 import github from '../images/github-logo.png'
 import instagram from '../images/instagram-logo.png'
 
@@ -18,6 +19,7 @@ const Footer = () => {
                 </ul>
                 <ul>
                     <a href="https://m.me/blkfltchr" style={{textDecoration: "none"}} target="_blank" rel="noopener noreferrer"><li>avoiding the <span style={{fontWeight: "bold", textDecoration: "underline"}}>Facebook</span> newsfeed;</li></a>
+                    <a href="https://www.linkedin.com/in/blakemfletcher/" style={{textDecoration: "none"}} target="_blank" rel="noopener noreferrer"><li>adding to the collective rolodex that is <span style={{fontWeight: "bold", textDecoration: "underline"}}>LinkedIn</span>;</li></a>
                     <a href="http://github.com/blkfltchr" style={{textDecoration: "none"}} target="_blank" rel="noopener noreferrer"><li>pushing and pulling on <span style={{fontWeight: "bold", textDecoration: "underline"}}>GitHub</span>;</li></a>
                     <a href="https://www.instagram.com/blkfltchr/" style={{textDecoration: "none"}} target="_blank" rel="noopener noreferrer"><li>and sharing photos on <span style={{fontWeight: "bold", textDecoration: "underline"}}>Instagram</span>.</li></a>
                 </ul>
@@ -28,6 +30,7 @@ const Footer = () => {
                 <a href="https://medium.com/@blakefletcher" target="_blank" rel="noopener noreferrer"><img style={{margin: "0.3rem", height: "25px"}} src={medium} alt="medium logo"/></a>
                 <a href="https://twitter.com/blkfltchr" target="_blank" rel="noopener noreferrer"><img style={{margin: "0.3rem", height: "25px"}} src={twitter} alt="twitter logo"/></a>
                 <a href="https://m.me/blkfltchr" target="_blank" rel="noopener noreferrer"><img style={{margin: "0.3rem", height: "25px"}} src={messenger} alt="messenger logo"/></a>
+                <a href="https://www.linkedin.com/in/blakemfletcher/" target="_blank" rel="noopener noreferrer"><img style={{margin: "0.3rem", height: "25px"}} src={linkedin} alt="messenger logo"/></a>
                 <a href="https://medium.com/@blakefletcher" target="_blank" rel="noopener noreferrer"><img style={{margin: "0.3rem", height: "25px"}} src={github} alt="github logo"/></a>
                 <a href="https://www.instagram.com/blkfltchr/" target="_blank" rel="noopener noreferrer"><img style={{margin: "0.3rem", height: "25px"}} src={instagram} alt="instagram logo"/></a>
             </div>
