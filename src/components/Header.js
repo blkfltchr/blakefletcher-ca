3a4f5c2a87b9df195efc0ac424cb20ee349fe630
Blake Fletcher <blkfltchr@gmail.com>
2018-12-09 15:44:01 -0500

    Converted Header styles to CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import '../css/component-styles.css'
 import logo from '../images/bf-logo.png'
 
 const Header = () => {
     return (
         <div>
-            <Link to="/"><img src={logo} alt="BlakeFletcher.ca logo" style={{height: "4rem", position: "absolute", top: "12px", left: "12px"}}/></Link>
-            <div style={{position: "absolute", top: "12px", right: "12px", height: "3rem", fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "1.2rem", display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
-                <Link style={{margin: "0 0.5rem"}} to="/"><span role="img" aria-label="house">🏠</span> Home</Link>
-                <Link style={{margin: "0 0.5rem"}} to="/projects"><span role="img" aria-label="laptop">💻</span> Projects</Link>
-                <a style={{margin: "0 0.5rem"}}  href="mailto:blkfltchr@gmail.com"><span role="img" aria-label="telephone">☎️</span> Contact</a>
+            <Link to="/"><img src={logo} alt="BlakeFletcher.ca logo" className="bf-logo"/></Link>
+            <div className="nav-links">
+                <Link className="nav-link" to="/"><span role="img" aria-label="house">🏠</span> Home</Link>
+                <Link className="nav-link" to="/projects"><span role="img" aria-label="laptop">💻</span> Projects</Link>
+                <a className="nav-link"  href="mailto:blkfltchr@gmail.com"><span role="img" aria-label="envelope">✉️</span> Contact</a>
             </div>
         </div>
     )
