6e6e63e7173a316e1b894dd1a1cce1469cbb1a76
Blake Fletcher <blkfltchr@gmail.com>
2018-12-09 23:54:43 -0500

    Added /experience page without content

---
+++ src/pages/experience.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Experience = () => {
+    return (
+        <div>
+            <Header />
+            <div style={{margin: "4.5rem auto 0 auto", height: "100%", textAlign: "center"}}>
+            <h2 style={{fontFamily: "Lato,Helvetica,Arial,Lucida,sans-serif", fontSize: "2rem"}}>Experience.</h2>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Experience
