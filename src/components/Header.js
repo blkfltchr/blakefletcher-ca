6e43ff526188280528bc1d710e38fe3918e93a6a
Blake Fletcher <blkfltchr@gmail.com>
2018-12-11 20:56:16 -0500

    Mobile menu working - kinda hacky

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,43 @@ import {Link} from 'gatsby'
 import '../css/header-styles.css'
 import logo from '../images/bf-logo.jpg'
 
-const Header = () => {
-    return (
-        <div>
-            <Link to="/"><img src={logo} alt="BlakeFletcher.ca logo" className="bf-logo"/></Link>
-            <div className="nav-links">
-                <div><Link className="nav-link" to="/"><span role="img" aria-label="house">🏠</span> Home</Link></div>
-                <div><Link className="nav-link" to="/projects"><span role="img" aria-label="laptop">💻</span> Projects</Link></div>
-                <div><Link className="nav-link" to="/experience"><span role="img" aria-label="backpack">🎒</span> Experience</Link></div>
-                <div><a className="nav-link" href="mailto:blkfltchr@gmail.com"><span role="img" aria-label="envelope">✉️</span> Contact</a></div>
-                <div><button style={{backgroundColor: "white", border: "none"}} href="javascript:void(0);" className="burger" onclick="myFunction()">
-                &#9776;
-                </button></div>
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuClassName: "nav-links nav-links-none",
+        }
+    }
+
+    handleToggleMenu = () => {
+        if (this.state.menuClassName = "nav-links nav-links-none") {
+            this.setState({
+                menuClassName: "nav-links"
+            })
+        } if (this.state.menuClassName != "nav-links nav-links-none") {
+            this.setState({
+                menuClassName: "nav-links nav-links-none"
+            })
+        }
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <Link to="/"><img src={logo} alt="BlakeFletcher.ca logo" className="bf-logo"/></Link>
+                <div className={this.state.menuClassName}>
+                    <div><Link className="nav-link" to="/"><span role="img" aria-label="house">🏠</span> Home</Link></div>
+                    <div><Link className="nav-link" to="/projects"><span role="img" aria-label="laptop">💻</span> Projects</Link></div>
+                    <div><Link className="nav-link" to="/experience"><span role="img" aria-label="backpack">🎒</span> Experience</Link></div>
+                    <div><a className="nav-link" href="mailto:blkfltchr@gmail.com"><span role="img" aria-label="envelope">✉️</span> Contact</a></div>
+                </div>
+                <div><a href="javascript:;" style={{backgroundColor: "white", border: "none", textDecoration: "none"}} className="burger" onClick={this.handleToggleMenu}>
+                    &#9776;
+                </a></div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Header
