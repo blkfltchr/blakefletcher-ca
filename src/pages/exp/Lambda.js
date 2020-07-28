7e25358fdcbb61c3a0b8b44f576c6896a758cb8a
Blake Fletcher <blkfltchr@gmail.com>
2020-07-27 21:57:02 -0400

    Updated main page and Inkblot description, added resume to nav

--- src/pages/exp/Lambda.js
+++ src/pages/exp/Lambda.js
@@ -24,7 +24,7 @@ const Lambda = () => (
                 <div className="exp">
                     <a href="https://lambdaschool.com/" target="_blank" rel="noopener noreferrer"><Img className="experience-img" sizes={data.file.childImageSharp.sizes} alt="lambda jumbo"/>
                     <h4>Lambda School</h4></a>
-                    <p className="left-paragraph">I started as a student at Lambda and decided to become a Project Manager to give back. The school has an amazing pay-it-forward culture that I help to uphold. Currently, I work as a PM for Lambda Labs, the capstone project portion of the school. I help graduates build the best applications they can.</p>
+                    <p className="left-paragraph">I started as a student at Lambda and decided to become a Project Manager to give back. The school has an amazing pay-it-forward culture that I help to uphold. I worked as a PM for Lambda Labs, the capstone project portion of the school. I helped graduates build the best applications they can.</p>
                     <p className="exp-dates">Team Lead, November 2018-June 2019</p>
                     <p className="exp-dates">Student, May 2018-November 2018</p>
                 </div>
